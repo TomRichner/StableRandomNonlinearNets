e1b5ae038ae9f1a5362a0736156be383efa05c50
Thomas Richner <ThomasJRichner@gmail.com>
2025-06-23 12:01:43 -0500

    updated set_path...m.  No longer adding fig directories to path

--- set_path_and_fig_defaults.m
+++ set_path_and_fig_defaults.m
@@ -1,12 +1,11 @@
-addpath(fileparts(mfilename('fullpath')));
+addpath(fileparts(mfilename('fullpath'))); % add the directory that set_path_and_fig_defaults.m is in
 addpath('supporting_functions');
 addpath('ode_solvers');
-addpath('fig_files')
-addpath(['fig_files' filesep 'IEDs'])
 addpath(['analytic_fixed_point_spectrum'])
-set(groot, 'DefaultFigureRenderer', 'painters');
+
+set(groot, 'DefaultFigureRenderer', 'painters'); % painters is better for SVG
 set(groot, 'DefaultAxesFontSize', 18);
 set(groot, 'DefaultTextFontSize', 16);
 set(groot, 'DefaultLineLineWidth', 2);
 set(groot, 'DefaultAxesLineWidth', 2);
-set_lines_no_red_cmap
+set_lines_no_red_cmap % remove red from the default lines colormap because red is reserved for inhibitory neurons
